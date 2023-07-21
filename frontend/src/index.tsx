cde0ed312de49a92924be3903a4db8dd5636d190
NellShark <krivolapovvladislav1998@gmail.com>
2023-07-21 23:54:01 +0600

    Redux-persist

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -6,17 +7,18 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ErrorBoundary } from '@components/ErrorBoundary';
 
-import { store } from '@store/store';
+import { persistor, store } from '@store/store';
 
 import { App } from './App';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ErrorBoundary>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>{' '}
       </Provider>
     </BrowserRouter>
   </ErrorBoundary>
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,18 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { trackPlayerReducer } from '@store/slices';
 import { userReducer } from '@store/slices/userSlice';
 
+const persistConfig = {
+  key: 'root',
+  blacklist: ['trackPlayer'],
+  storage
+};
+
+const rootReducer = combineReducers({
+  trackPlayer: trackPlayerReducer,
+  user: userReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    trackPlayer: trackPlayerReducer,
-    user: userReducer
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false
     })
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
